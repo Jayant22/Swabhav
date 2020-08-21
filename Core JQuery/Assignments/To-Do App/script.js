9d983c7e232a70fc96f577d7c49c726fdc55093c
lenovo <jayantkodam2223@gmail.com>
2020-08-21 23:12:28 +0530

    Assignments Day 38

--- Core JQuery/Assignments/To-Do App/script.js
+++ Core JQuery/Assignments/To-Do App/script.js
@@ -1,45 +1,96 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-	$('#list-items').html(localStorage.getItem('listItems'));
-    
-	$('.add-items').submit(function(event) 
-	{
-		event.preventDefault();
+	var items = getFromLocal('memos');
+	var times = getFromLocal('times');
+	var index;
 
-		var item = $('#todo-list-item').val();
-
-		if(item) 
-		{
-			$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
-			
-			localStorage.setItem('listItems', $('#list-items').html());
-			
-			$('#todo-list-item').val("");
+	loadList(items);
+	
+	$('button').prop('disabled', true);
+	$('input').keyup(function(){
+		if($(this).val().length !== 0) {
+			$('button').prop('disabled', false);
+		} else {
+			$('button').prop('disabled', true);
 		}
-		
+	});
+	
+	$('#main-input').keypress(function(e){
+		if(e.which === 13) {
+			if ($('#main-input').val().length !== 0)
+				$('#main-button').click();
+		}
+	});
+	
+	$('#main-button').click(function(){
+		var value = $('#main-input').val();
+		const time = moment().format('MM D YYYY hh:mm:ss');
+
+		items.push(value);
+		times.push(time);
+
+		//console.log(items[0]);
+		$('#main-input').val('');
+		loadList(items);
+		storeToLocal('memos', items);
+		storeToLocal('times', times);
+		// set button to 
+		$('button').prop('disabled', true);
+	});
+	
+	$('ul').delegate("span", "click", function(event){
+		event.stopPropagation();
+		index = $('span').index(this);
+		$('li').eq(index).remove();
+		items.splice(index, 1);
+		times.splice(index, 1);
+		storeToLocal('memos', items);
+		storeToLocal('times', times);
 	});
 
-	$(document).on('change', '.checkbox', function() 
-	{
-		if($(this).attr('checked')) 
-		{
-			$(this).removeAttr('checked');
-		} 
+	$('ul').delegate('li', 'click', function(){
+		index = $('li').index(this);
+		var content = items[index];
+		console.log(content);
+		$('#edit-input').val(content );
+	});
+
+	$('#edit-button').click(function(){
+		items[index] = $('#edit-input').val();
+		loadList(items);
+		storeToLocal("memos", items);
+	});
+
+	function loadList(items){
+		$('li').remove();
+		if(items.length > 0) {
+			for(var i = 0; i < items.length; i++) {
+
+				let timeDifference = calculateTimeDifference(times[i]);
+
+				$('ul').append('<li class= "list-group-item" data-toggle="modal" data-target="#editModal">' 
+				+ items[i] + " " + times[i] + " " + timeDifference + 
+				'<span class="glyphicon glyphicon-remove"></span</li>');
+			}
+		}
+	};
+
+	function calculateTimeDifference(time) {
+        let taskAddedTime = moment(time);
+        let currentTime = moment(moment().format('MM D YYYY hh:mm:ss'));
+        let compare = moment.duration(taskAddedTime.diff(currentTime));
+        return compare.humanize(true);
+	}
+	
+	function storeToLocal(key, items){
+		localStorage[key] = JSON.stringify(items);
+	}
+
+	function getFromLocal(key){
+		if(localStorage[key])
+			return JSON.parse(localStorage[key]);
 		else 
-		{
-			$(this).attr('checked', 'checked');
-		}
-
-		$(this).parent().toggleClass('completed');
-		
-		localStorage.setItem('listItems', $('#list-items').html());
-	});
-
-	$(document).on('click', '.remove', function() 
-	{
-		$(this).parent().remove();
-		
-		localStorage.setItem('listItems', $('#list-items').html());
-	});
+			return [];
+	}
 
 });
